527e263f3e3bf85575be4c92a37dd1748aa3e407
Oleksii Kulikov <yeexel@gmail.com>
2019-01-10 23:35:52 +0100

    up cf

--- cloudfront.tf
+++ cloudfront.tf
@@ -13,7 +13,7 @@ resource "aws_cloudfront_distribution" "site" {
     custom_origin_config {
       http_port              = "80"
       https_port             = "443"
-      origin_protocol_policy = "http-only"
+      origin_protocol_policy = "match-viewer"
       origin_ssl_protocols   = ["TLSv1", "TLSv1.1", "TLSv1.2"]
     }
   }
@@ -51,10 +51,11 @@ resource "aws_cloudfront_distribution" "site" {
       }
     }
 
-    viewer_protocol_policy = "allow-all"
-    min_ttl                = 0
-    default_ttl            = "${var.cache_default_ttl}"
-    max_ttl                = "${var.cache_max_ttl}"
+    viewer_protocol_policy = "redirect-to-https"
+
+    # min_ttl                = 0
+    # default_ttl            = "${var.cache_default_ttl}"
+    # max_ttl                = "${var.cache_max_ttl}"
   }
 
   restrictions {
